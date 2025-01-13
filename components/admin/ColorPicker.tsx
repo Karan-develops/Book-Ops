175145e9425f180fdebc17e0803ad73879102fdd
Karan <mrkaran2k5@gmail.com>
2025-01-13 18:27:51 +0530

    Admin Pages

---
+++ components/admin/ColorPicker.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface Props {
+    value?: string;
+    onPickerChange: (color: string) => void;
+  }
+
+const ColorPicker = ({ value, onPickerChange }: Props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ColorPicker
